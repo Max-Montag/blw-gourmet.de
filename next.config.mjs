77e4ed8403acc740992bfe0daab268baf62fb27d
MaMon94 <max.montag1994@gmail.com>
2024-10-19 02:21:57 +0200

    update next conf

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: NEXT_PUBLIC_IMAGE_PROTOCOL,
         hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME,
         port: "8000",
         pathname: "/api/**",
