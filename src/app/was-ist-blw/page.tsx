65db5f99d51d25a1ad8f009972aff6193fd09643
MaMon94 <max.montag1994@gmail.com>
2024-10-22 13:24:36 +0200

    remove semicolon

---
+++ src/app/was-ist-blw/page.tsx
@@ -0,0 +1,9 @@
+import WhatIsBlw from "@/components/what-is-blw/WhatIsBlw";
+
+export default async function WhatIsBlwPage() {
+  return (
+  <div className="py-12">
+    <WhatIsBlw />
+  </div>
+  );
+}
